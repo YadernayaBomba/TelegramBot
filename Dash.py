839c3b14ee763f5af94d42e1ab4cddcb24977a12
glebc <glebchikgrub@gmail.com>
2023-12-24 01:21:06 +1000

    1.3

--- Dash.py
+++ Dash.py
@@ -25,6 +25,24 @@ pie_chart_figure = px.pie(df, names='Status')
 # Создаем гистограмму
 histogram = px.histogram(df, x='Category')
 
+# Создаем таблицу
+data_table = dash_table.DataTable(
+    columns=[{"name": i, "id": i} for i in df.columns],
+    data=df.to_dict('records'),
+    page_size=10,  # Количество строк на странице
+    style_table={'height': '300px', 'overflowY': 'auto'},
+    style_cell={
+        'textAlign': 'left',
+        'padding': '10px',
+        'minWidth': '100px', 'width': '100px', 'maxWidth': '100px',
+        'whiteSpace': 'normal'
+    },
+    style_header={
+        'backgroundColor': 'rgb(230, 230, 230)',
+        'fontWeight': 'bold'
+    }
+)
+
 # Запускаем веб-приложение
 if __name__ == '__main__':
     app.run_server(debug=True)
