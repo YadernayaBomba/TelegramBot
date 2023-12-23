7270501ffebb62240f22b619fd9a2bdb77dbadb8
glebc <glebchikgrub@gmail.com>
2023-12-24 01:05:20 +1000

    first step

---
+++ Bot.py
@@ -0,0 +1,10 @@
+import telebot
+from telebot import types
+
+
+TOKEN = '6540302676:AAHr2m6er2wf1VDiMW8pQEJf6_1fLRzz_n0'
+
+bot = telebot.TeleBot(TOKEN)
+
+# Словарь с данными пользователей
+user_data = {}
