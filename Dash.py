6afd2c50c8d82c8b660613755c69cb009f35a96c
glebc <glebchikgrub@gmail.com>
2023-12-24 01:20:41 +1000

    1.2

--- Dash.py
+++ Dash.py
@@ -15,6 +15,16 @@ df['ExpirationDate'] = pd.to_datetime(df['ExpirationDate'], errors='coerce')
 # Создаем веб-приложение
 app = dash.Dash(__name__)
 
+# Группируем данные по годам и создаем временную диаграмму
+grouped = df.groupby(df['StartDate'].dt.year)['Participants'].sum().reset_index()
+time_series_figure = px.line(grouped, x='StartDate', y='Participants', title='Количество участников по годам')
+
+# Создаем круговую диаграмму
+pie_chart_figure = px.pie(df, names='Status')
+
+# Создаем гистограмму
+histogram = px.histogram(df, x='Category')
+
 # Запускаем веб-приложение
 if __name__ == '__main__':
     app.run_server(debug=True)
