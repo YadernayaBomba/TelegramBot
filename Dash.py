16e6b0a57ee8b74d4168bf19096f47f6536006d8
glebc <glebchikgrub@gmail.com>
2023-12-24 01:20:16 +1000

    1.1

--- Dash.py
+++ Dash.py
@@ -7,3 +7,14 @@ import dash_table
 # Считываем данные из CSV-файла
 file_path = 'Данные.csv'
 df = pd.read_csv(file_path)
+
+# Преобразуем столбы в формат datetime
+df['StartDate'] = pd.to_datetime(df['StartDate'])
+df['ExpirationDate'] = pd.to_datetime(df['ExpirationDate'], errors='coerce')
+
+# Создаем веб-приложение
+app = dash.Dash(__name__)
+
+# Запускаем веб-приложение
+if __name__ == '__main__':
+    app.run_server(debug=True)
