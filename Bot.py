0dcba8c8ebea2f7a5061b010cade16818ac64532
glebc <glebchikgrub@gmail.com>
2023-12-24 01:11:30 +1000

    1.2

--- Bot.py
+++ Bot.py
@@ -27,3 +27,42 @@ def send_welcome(message):
     markup.add(item1, item2, item3, item4)
     bot.send_message(message.chat.id, f"Здравствуйте {message.from_user.first_name}!\n\nЗдесь вы можете записаться, чтобы принять участие в проекте или предложить свой. Выберите регистрацию или напишите /register. После регистрации с вами свяжется один из наших сотрудников.\n"
                                       "\nТакже вы можете посмотреть актуальную информацию по проектам, для этого выберите диаграммы или напишите /diagrams'", reply_markup=markup)
+
+
+@bot.message_handler(func=lambda message: message.text == "Перезапуск")
+def restart(message):
+    user_id = message.from_user.id
+    user_data[user_id] = {'name': '', 'age': '', 'phone': ''}
+    send_welcome(message)
+
+
+@bot.message_handler(func=lambda message: message.text == "Регистрация" or message.text == "/register")
+def registration(message):
+    bot.send_message(message.chat.id, "Введите ваше ФИО:")
+    bot.register_next_step_handler(message, get_name)
+
+
+def get_name(message):
+    user_id = message.from_user.id
+    user_data[user_id]['name'] = message.text
+    bot.send_message(message.chat.id, "Введите ваш возраст:")
+    bot.register_next_step_handler(message, get_age)
+
+
+def get_age(message):
+    try:
+        age = int(message.text)
+        user_id = message.from_user.id
+        user_data[user_id]['age'] = age
+        bot.send_message(message.chat.id, "Введите ваш номер телефона:")
+        bot.register_next_step_handler(message, get_phone)
+    except ValueError:
+        bot.send_message(message.chat.id, "Пожалуйста, введите корректный возраст (целое число):")
+        bot.register_next_step_handler(message, get_age)
+
+
+def get_phone(message):
+    user_id = message.from_user.id
+    user_data[user_id]['phone'] = message.text
+    bot.send_message(message.chat.id, "Регистрация завершена. Ваши данные:")
+    show_data(message)
